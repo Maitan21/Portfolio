8df61ae2e3eff04b9287b1eff538c34e9c0a8601
Jho <jho@Jho.local>
2020-07-19 16:40:16 +0900

    change profiel

--- form/contact.php
+++ form/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <sender@yourdomain.com>';
+$from = "oklfg20000@naver.com";
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <reciever@example.com>';
+$sendTo = "oklfg2002@gmail.com";
 
 // subject of the email
 $subject = 'New message from contact form';
